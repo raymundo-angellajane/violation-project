fc92a6885f45c440a594a7954338ea7ad4be8b58
raymundo-angellajane <angellajaneraymundo05@gmail.com>
2025-08-21 18:30:43 +0800

    add student view route

--- violationProject/routes/web.php
+++ violationProject/routes/web.php
@@ -17,8 +17,6 @@ Route::resource('violations', ViolationController::class)->except(['show']);
 Route::get('/violations/export-pdf', [ViolationController::class, 'exportPdf'])
     ->name('violations.exportPdf');
 
-Route::get('/appeals/create/{violation}', [AppealController::class, 'create'])->name('appeals.create');
-
-Route::post('/appeals/store', [AppealController::class, 'store'])->name('appeals.store');
-
 Route::get('/student/violations', [StudentViolationController::class, 'index'])->name('student.violations');
+
+Route::post('/student/appeal', [StudentViolationController::class, 'submitAppeal'])->name('student.appeal');
