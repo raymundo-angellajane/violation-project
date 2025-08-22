67b5dd66a09d9e06c4d87eaeeefd37d99cf61e8f
raymundo-angellajane <angellajaneraymundo05@gmail.com>
2025-08-22 17:55:31 +0800

    delete duplicate model file

--- violationProject/app/Models/StudentAppeal.php
+++ violationProject/app/Models/StudentAppeal.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StudentAppeal extends Model
 {
-    //
+     use HasFactory;
+
+    protected $fillable = [
+        'student_appeals_id',
+        'student_id',
+        'violation_id',
+        'appeals_id',
+        'status',
+        'reviewed_by',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
